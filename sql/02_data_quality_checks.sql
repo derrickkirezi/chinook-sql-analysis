def7b9d97a69a7978b984cbf15901a0e25a4633f
derrickkirezi <134754603+derrickkirezi@users.noreply.github.com>
2026-01-22 16:55:00 +0200

    Update 02_data_quality_checks.sql

--- sql/02_data_quality_checks.sql
+++ sql/02_data_quality_checks.sql
@@ -12,40 +12,33 @@
   -- Overall, the database is consistent. The only issue is that some tracks have never been sold, which may be due to low demand or newly introduced products.
 
 
-USE Chinook;
+USE chinook;
 
--- 1. Customers with no invoices
-SELECT 
-    c.CustomerId,
-    c.FirstName,
-    c.LastName
-FROM customers c
-LEFT JOIN invoices i 
-    ON c.CustomerId = i.CustomerId
-WHERE i.InvoiceId IS NULL;
+--1. Check for customers with no invoices
 
--- 2. Invoices with no items
-SELECT 
-    i.InvoiceId,
-    i.InvoiceDate,
-    i.Total
-FROM invoices i
-LEFT JOIN InvoiceLine il
-    ON i.InvoiceId = il.InvoiceId
-WHERE il.InvoiceLineId IS NULL;
+select c.FirstName, c.LastName, i.InvoiceId
+from customer c
+left join invoice i on c.CustomerId = i.CustomerId
+where InvoiceId is null;
 
--- 3. Tracks that were never sold
-SELECT 
-    t.TrackId,
-    t.Name
-FROM tracks t
-LEFT JOIN InvoiceLine il
-    ON t.TrackId = il.TrackId
-WHERE il.TrackId IS NULL;
+--2. Check for invoices with no invoice items
 
--- 4. Invoices with zero or negative totals
-SELECT 
-    InvoiceId,
-    Total
-FROM invoices
-WHERE Total <= 0;
+select i.InvoiceId, il.InvoiceLineId
+from invoice i
+left join invoiceline il 
+on i.InvoiceId = il.InvoiceId
+where InvoiceLineId is null;
+
+--3. Tracks that were never sold
+
+select t.TrackId, t.Name, il.InvoiceId
+from track t
+left join invoiceline il
+on t.TrackId = il.TrackId
+where InvoiceId is null;
+
+--4. Invoices with zero or negative totals
+
+select InvoiceId, Total
+from invoice
+where Total <= 0;
